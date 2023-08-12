5949e5df6600b3cd13e06b20c916138bcaa56530
Kusal Kalinga <kusalkalingainfo@gmail.com>
2023-08-13 00:08:06 +0530

    Authentication added

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,90 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { TaskStatus } from './task-status.enum';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTaskFilterDto } from './dto/get-task-filter.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Task } from './task.entity';
+import { TasksRepository } from './tasks.repository';
+import { User } from 'src/auth/user.entity';
+
+@Injectable()
+export class TasksService {
+  constructor(
+    @InjectRepository(Task)
+    private tasksRepository: TasksRepository,
+  ) {}
+
+  //create a task
+  async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
+    const { title, description } = createTaskDto;
+
+    const task = this.tasksRepository.create({
+      title,
+      description,
+      status: TaskStatus.OPEN,
+      user,
+    });
+
+    await this.tasksRepository.save(task);
+    return task;
+  }
+
+  // get task by id
+  async getTaskById(id: string, user: User): Promise<Task> {
+    const found = await this.tasksRepository.findOne({
+      where: { id: id, user: user },
+    });
+
+    if (!found) {
+      throw new NotFoundException(`Task with ID "${id}" not found`);
+    }
+
+    return found;
+  }
+
+  //delete task by id
+  async deleteTaskById(id: string , user : User): Promise<void> {
+    const result = await this.tasksRepository.delete({id , user});
+
+   
+
+    if (result.affected === 0) {
+      throw new NotFoundException(`Task with ID "${id}" not found`);
+    }
+  }
+
+  //update task status by id
+  async updateTaskStatus(
+    id: string,
+    status: TaskStatus,
+    user: User,
+  ): Promise<Task> {
+    const task = await this.getTaskById(id, user);
+
+    task.status = status;
+
+    await this.tasksRepository.save(task);
+
+    return task;
+  }
+
+  async getTasks(filterDto: GetTaskFilterDto, user: User): Promise<Task[]> {
+    const { status, search } = filterDto;
+
+    const query = this.tasksRepository.createQueryBuilder('task');
+    query.where({ user });
+    if (status) {
+      query.andWhere('task.status = :status', { status });
+    }
+
+    if (search) {
+      query.andWhere(
+        '(LOWER(task.title) LIKE LOWER(:search)  OR LOWER(task.description) LIKE LOWER(:search))',
+        { search: `%${search}%` },
+      );
+    }
+
+    const tasks = await query.getMany();
+    return tasks;
+  }
+}
